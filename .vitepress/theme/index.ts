8a9356b2cb814763effa27edac15a009a061ea62
ihavecoke <mixboip@gmail.com>
2025-05-26 21:56:12 +0800

    Add `medium-zoom` support for release notes.

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,9 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import Footer from '../../docs/pages/Footer.vue'
+import mediumZoom from "medium-zoom";
+import { useRoute } from "vitepress";
+import { onMounted, watch, nextTick } from "vue";
 import './custom.css'
 
 export default {
@@ -11,5 +14,20 @@ export default {
         "layout-bottom": () => h(Footer)
       }
     )
+  },
+  setup() {
+    // Support zooming images
+    const route = useRoute();
+    const initZoom = () => {
+      new mediumZoom(".content-container .main img", { background: "#dddde3" });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom()),
+    );
+
   }
 }
