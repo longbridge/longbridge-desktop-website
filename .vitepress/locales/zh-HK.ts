be4bd1b79a90f2591632f76ff30235d6bbb445e5
Jason Lee <huacnlee@gmail.com>
2025-06-12 15:58:11 +0800

    Update zh-HK.ts

--- .vitepress/locales/zh-HK.ts
+++ .vitepress/locales/zh-HK.ts
@@ -69,34 +69,34 @@ export default {
         tabs: {
             tab_0: {
                 title: "智能異動監測",
-                description: "毫秒級預警自選股異動，機會從不漏網",
+                description: "毫秒級預警自選股異動，機會從不漏網。",
                 image: "https://assets.lbctrl.com/uploads/3c70e18d-12f2-4287-8f45-9ad5658fcba1/real-time.png",
             },
             tab_1: {
                 title: "直觀熱力圖譜",
-                description: "全市場行情一目了然，熱點盡在掌握",
+                description: "全市場行情一目了然，熱點盡在掌握。",
                 image: "https://assets.lbctrl.com/uploads/5a1afc15-98dc-4757-9396-65dd872d4b61/real-time-2.png",
             },
             tab_2: {
                 title: "主力資金追蹤",
-                description: "大單動向實時解析，迅速把握主力動向",
+                description: "大單動向實時解析，迅速把握主力動向。",
             },
         },
     },
     globalTrade: {
         title: "全球極速交易<br />多品種一鍵搞定",
         items: [
-            "港股/美股/A 股/新加坡股，一屏觸達全球市場",
-            "做多/美股做空/美股期權/港股窩輪牛熊證，豐富工具把握機遇",
-            "開啟智能交易面板，下單快人一步，持倉實時監控，交易顯著提效"
+            "港股、美股、A 股、新加坡股，一屏觸達全球市場。",
+            "做多、美股做空、美股期權、港股窩輪牛熊證，豐富工具把握機遇。",
+            "開啟智能交易面板，下單快人一步，持倉實時監控，交易顯著提效。"
         ],
     },
     smartNav: {
         button: "智能導航",
         button_2: "歷史記錄",
         title: "智能導航系統<br />市場動態一觸即達",
-        description: "獨創導航欄標籤頁，重要股票秒查報價",
-        description_2: "一鍵回溯重要資訊，歷史記錄隨時調取",
+        description: "獨創導航欄標籤頁，重要股票秒查報價。",
+        description_2: "一鍵回溯重要資訊，歷史記錄隨時調取。",
     },
     multiPlatform: {
         title: "多端支援，專業盯盤更高效",
