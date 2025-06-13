784ecdf31b1bdb6d10423d1a20741e4f62c6e980
tzyoo <37643993+Tzyito@users.noreply.github.com>
2025-06-13 17:18:33 +0800

    Fix open the multilingual link (#45)

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,7 +2,7 @@ import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme-without-fonts'
 import Footer from '../../docs/pages/Footer.vue'
 import mediumZoom from "medium-zoom";
-import { useRoute, useRouter, withBase } from "vitepress";
+import { useRoute, useRouter } from "vitepress";
 import { onMounted, watch, nextTick } from "vue";
 import './custom.css'
 import 'virtual:uno.css'
@@ -39,13 +39,7 @@ export default {
 
     const lang = getLocaleByAppUA();
     if (lang) {
-      const map = {
-        'en': 'en',
-        'zh-CN': 'zh-CN',
-        'zh-HK': 'zh-HK',
-      }
-      console.log(withBase(map[lang]));
-      router.go(`${withBase(map[lang])}`);
+      router.go(`/desktop/${lang}/`);
     }
   }
 }
