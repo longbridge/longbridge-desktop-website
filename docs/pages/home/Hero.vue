db07af9c9a49a81a592ed75d46bdea04b7aa0637
tzyoo <37643993+Tzyito@users.noreply.github.com>
2025-06-20 18:10:03 +0800

    fix: home layout style and add new image (#58)

--- docs/pages/home/Hero.vue
+++ docs/pages/home/Hero.vue
@@ -6,17 +6,15 @@ const { name, tagline } = useLocale();
 </script>
 <template>
     <div class="max-w-[1200px] mx-auto">
-        <div class="flex flex-col items-center justify-center mb-8">
+        <div class="flex flex-col items-center justify-center mb-5">
             <div class="relative">
                 <h1
-                    class="sm:!text-4xl md:!text-5xl lg:!text-6xl font-bold text-black mb-4 flex !items-baseline justify-center gap-4"
-                >
+                    class="sm:!text-4xl md:!text-5xl lg:!text-6xl font-bold text-black mb-4 flex !items-baseline justify-center gap-4 relative">
                     {{ name }}
+                    <span
+                        class="absolute bottom-7px -right-[calc(3rem+12px)] lg:-right-[calc(5.25rem+12px)] flex items-center justify-center w-12 h-5 lg:w-21 lg:h-8 bg-black text-white px-3 py-1 rounded-full text-xs md:text-sm lg:text-lg font-bold">NEW</span>
+
                 </h1>
-                <span
-                    class="absolute top-[16px] -right-[calc(3rem+12px)] lg:-right-[calc(5.25rem+12px)] flex items-center justify-center w-12 h-5 lg:w-21 lg:h-8 bg-black text-white px-3 py-1 rounded-full text-xs md:text-sm lg:text-lg font-bold"
-                    >NEW</span
-                >
             </div>
             <p class="max-w-2xl mx-auto text-lg text-[var(--lb-gray-1)]">
                 {{ tagline }}
@@ -24,19 +22,12 @@ const { name, tagline } = useLocale();
         </div>
 
         <!-- Download Buttons -->
-        <DownloadInfo class="mb-6.5" />
+        <DownloadInfo class="mb-1" />
 
         <!-- Main App Screenshot -->
-        <div
-            class="w-auto mx-auto lg:w-5xl px-0 lg:px-2"
-            id="hero-image"
-            v-motion="motionVisible(300, 'visible')"
-        >
-            <img
-                src="https://assets.lbctrl.com/uploads/8d685eb9-abd4-4297-9f3d-a22bf4e98e78/hero-image.png"
-                alt="Longbridge Pro Interface"
-                class="w-full h-auto rounded-lg"
-            />
+        <div class="w-auto mx-auto lg:w-5xl px-0 lg:px-2" id="hero-image" v-motion="motionVisible(300, 'visible')">
+            <img src="https://assets.lbctrl.com/uploads/8d685eb9-abd4-4297-9f3d-a22bf4e98e78/hero-image.png"
+                alt="Longbridge Pro Interface" class="w-full h-auto rounded-lg" />
         </div>
     </div>
 </template>
--- docs/pages/home/SmartNav.vue
+++ docs/pages/home/SmartNav.vue
@@ -4,12 +4,14 @@ import { useLocale } from "../utils";
 import { useIntersectionObserver } from "@vueuse/core";
 
 const { smartNav } = useLocale();
-const video = useTemplateRef("video");
+const video = useTemplateRef('video');
 useIntersectionObserver(video, ([{ isIntersecting }]) => {
-    if (isIntersecting && video.value.paused) {
-        video.value.play();
-    } else {
-        video.value.pause();
+    if (video.value) {
+        if (isIntersecting && video.value.paused) {
+            video.value.play();
+        } else {
+            video.value.pause();
+        }
     }
 });
 // Props 定义
@@ -24,64 +26,57 @@ const activeTab = ref(0);
 // 简化的内容配置
 const content = {
     title: smartNav.title,
-    features: [smartNav.description, smartNav.description_2],
-};
+    features: [
+        smartNav.description,
+        smartNav.description_2
+    ],
+}
 const handleTabClick = (index) => {
     activeTab.value = index;
     if (video.value && video.value.paused) {
         video.value.play();
     }
-};
+}
 </script>
 
 <template>
     <div class="bg-gray-50">
         <section class="py-16 lg:py-24 px-8">
             <div class="max-w-[1200px] mx-auto">
-                <div
-                    class="flex lt-sm:(flex-col) flex-row justify-between items-center bg-white rounded-xl"
-                >
+                <div class="flex lt-sm:(flex-col) flex-row justify-between items-center bg-white rounded-xl">
                     <!-- 左侧内容 -->
                     <div class="space-y-8 flex flex-col justify-between h-full">
                         <div class="pt-12 px-12 lt-sm:(px-8 pt-4)">
-                            <h1
-                                class="text-4xl lg:text-5xl xl:text-6xl font-bold text-black leading-tight"
-                            >
+                            <h1 class="text-4xl lg:text-5xl xl:text-6xl font-bold text-black leading-tight">
                                 <span v-html="content.title"></span>
                             </h1>
                         </div>
 
                         <!-- 特性列表 -->
                         <div class="space-y-6 pb-12 lt-sm:(pb-4)">
-                            <div
-                                v-for="(feature, index) in content.features"
-                                :key="index"
+                            <div v-for="(feature, index) in content.features" :key="index"
                                 :data-active="activeTab === index"
                                 class="space-y-1 pl-8 lg:pl-12 relative hover:cursor-pointer data-[active='true']:(after:bg-black) after:(content-[''] w-3px h-full bg-[var(--lb-gray-1)] absolute left-0 top-0)"
-                                @click="handleTabClick(index)"
-                            >
-                                <div
-                                    :data-active="activeTab === index"
+                                @click="handleTabClick(index)">
+                                <div :data-active="activeTab === index"
                                     class="text-xl lt-sm:(text-sm) text-[var(--lb-gray-1)] data-[active='true']:(text-black font-bold)"
-                                    v-html="feature"
-                                ></div>
+                                    v-html="feature"></div>
                             </div>
                         </div>
                     </div>
 
                     <!-- 右侧图片 -->
-                    <div
-                        class="flex justify-center lg:justify-end py-4 px-4 lg:pl-0"
-                    >
+                    <div class="flex justify-center lg:justify-end py-4 px-4 lg:pl-0">
                         <div class="relative max-w-3xl w-full">
-                            <video
-                                ref="video"
-                                playsinline="true"
-                                webkit-playsinline="true"
-                                muted="true"
-                                src="https://assets.lbctrl.com/uploads/7f24579e-1c9c-4958-8af5-c2aa294d9498/desktop_en.mp4"
-                                class="rounded-lg w-83 h-46 lg:w-160 lg:h-90"
-                            ></video>
+                            <template v-if="activeTab === 0">
+                                <video ref="video" playsinline="true" webkit-playsinline="true" muted="true"
+                                    src="https://assets.lbctrl.com/uploads/7f24579e-1c9c-4958-8af5-c2aa294d9498/desktop_en.mp4"
+                                    class="rounded-lg w-83 max-h-46 lg:w-160 lg:max-h-90"></video>
+                            </template>
+                            <template v-else>
+                                <img src="https://assets.lbctrl.com/uploads/795f8f25-3ee0-4cb8-ba74-9c42f5a2cb72/smart-2.png"
+                                    class="rounded-lg w-83 max-h-46 lg:w-160 lg:max-h-90">
+                            </template>
                         </div>
                     </div>
                 </div>
