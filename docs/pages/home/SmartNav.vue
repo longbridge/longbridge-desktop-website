ec8ae0e8221b9cd2b7975de16a8e02f895f9d144
ihavecoke <mixboip@gmail.com>
2025-12-08 17:35:29 +0800

    .

--- docs/pages/home/SmartNav.vue
+++ docs/pages/home/SmartNav.vue
@@ -4,94 +4,107 @@ import { useLocale } from "../utils";
 import { useIntersectionObserver } from "@vueuse/core";
 
 const { smartNav } = useLocale();
-const video = useTemplateRef('video');
+const video = useTemplateRef("video");
 useIntersectionObserver(video, ([{ isIntersecting }]) => {
-    if (video.value) {
-        if (isIntersecting && video.value.paused) {
-            video.value.play();
-        } else {
-            video.value.pause();
-        }
+  if (video.value) {
+    if (isIntersecting && video.value.paused) {
+      video.value.play();
+    } else {
+      video.value.pause();
     }
+  }
 });
 // Props 定义
 const props = defineProps({
-    stickyOffset: {
-        type: Number,
-        default: 64,
-    },
+  stickyOffset: {
+    type: Number,
+    default: 64,
+  },
 });
 
 const activeTab = ref(0);
 // 简化的内容配置
 const content = {
-    title: smartNav.title,
-    features: [
-        smartNav.description,
-        smartNav.description_2
-    ],
-}
+  title: smartNav.title,
+  features: [smartNav.description, smartNav.description_2],
+};
 const handleTabClick = (index) => {
-    activeTab.value = index;
-    if (video.value && video.value.paused) {
-        video.value.play();
-    }
-}
+  activeTab.value = index;
+  if (video.value && video.value.paused) {
+    video.value.play();
+  }
+};
 </script>
 
 <template>
-    <div class="bg-gray-50">
-        <section class="py-16 lg:py-24 px-8">
-            <div class="max-w-[1200px] mx-auto">
-                <div class="flex lt-sm:(flex-col) flex-row justify-between items-center bg-white rounded-xl">
-                    <!-- 左侧内容 -->
-                    <div class="space-y-8 flex flex-col justify-between h-full">
-                        <div class="pt-12 px-12 lt-sm:(px-8 pt-4)">
-                            <h1 class="text-4xl lg:text-5xl xl:text-6xl font-bold text-black leading-tight">
-                                <span v-html="content.title"></span>
-                            </h1>
-                        </div>
-
-                        <!-- 特性列表 -->
-                        <div class="space-y-6 pb-12 lt-sm:(pb-4)">
-                            <div v-for="(feature, index) in content.features" :key="index"
-                                :data-active="activeTab === index"
-                                class="space-y-1 pl-8 lg:pl-12 relative hover:cursor-pointer data-[active='true']:(after:bg-black) after:(content-[''] w-3px h-full bg-[var(--lb-gray-1)] absolute left-0 top-0)"
-                                @click="handleTabClick(index)">
-                                <div :data-active="activeTab === index"
-                                    class="text-xl lt-sm:(text-sm) text-[var(--lb-gray-1)] data-[active='true']:(text-black font-bold)"
-                                    v-html="feature"></div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- 右侧图片 -->
-                    <div class="flex justify-center lg:justify-end py-4 px-4 lg:pl-0">
-                        <div class="relative max-w-3xl w-full">
-                            <template v-if="activeTab === 0">
-                                <video ref="video" playsinline="true" webkit-playsinline="true" muted="true"
-                                    src="https://assets.lbctrl.com/uploads/7f24579e-1c9c-4958-8af5-c2aa294d9498/desktop_en.mp4"
-                                    class="rounded-lg w-83 max-h-46 lg:w-160 lg:max-h-90"></video>
-                            </template>
-                            <template v-else>
-                                <img src="https://assets.lbctrl.com/uploads/3ad53fca-8014-442b-9c75-81c87458e4bb/smart-2.png"
-                                    class="rounded-lg w-83 max-h-46 lg:w-160 lg:max-h-90">
-                            </template>
-                        </div>
-                    </div>
-                </div>
+  <div class="bg-gray-50">
+    <section class="py-16 lg:py-24 px-8">
+      <div class="max-w-[1200px] mx-auto">
+        <div
+          class="flex lt-sm:(flex-col) flex-row justify-between items-center bg-white rounded-xl"
+        >
+          <!-- 左侧内容 -->
+          <div class="space-y-8 flex flex-col justify-between h-full">
+            <div class="pt-12 px-12 lt-sm:(px-8 pt-4)">
+              <h1
+                class="text-4xl lg:text-5xl xl:text-6xl font-bold text-black leading-tight"
+              >
+                <span v-html="content.title"></span>
+              </h1>
             </div>
-        </section>
-    </div>
+
+            <!-- 特性列表 -->
+            <div class="space-y-6 pb-12 lt-sm:(pb-4)">
+              <div
+                v-for="(feature, index) in content.features"
+                :key="index"
+                :data-active="activeTab === index"
+                class="space-y-1 pl-8 lg:pl-12 relative hover:cursor-pointer data-[active='true']:(after:bg-black) after:(content-[''] w-3px h-full bg-[var(--lb-gray-1)] absolute left-0 top-0)"
+                @click="handleTabClick(index)"
+              >
+                <div
+                  :data-active="activeTab === index"
+                  class="text-xl lt-sm:(text-sm) text-[var(--lb-gray-1)] data-[active='true']:(text-black font-bold)"
+                  v-html="feature"
+                ></div>
+              </div>
+            </div>
+          </div>
+
+          <!-- 右侧图片 -->
+          <div class="flex justify-center lg:justify-end py-4 px-4 lg:pl-0">
+            <div class="relative max-w-3xl w-full">
+              <template v-if="activeTab === 0">
+                <video
+                  ref="video"
+                  playsinline="true"
+                  webkit-playsinline="true"
+                  muted="true"
+                  src="https://assets.lbctrl.com/uploads/cf38bebc-96a5-42cb-9c37-f1b6de1be240/stock-navgation.mp4"
+                  class="rounded-lg w-83 max-h-46 lg:w-160 lg:max-h-90"
+                ></video>
+              </template>
+              <template v-else>
+                <img
+                  src="https://assets.lbctrl.com/uploads/68e4f451-9fc5-43ba-80da-72018cf8d091/global-history.webp"
+                  class="rounded-lg w-83 max-h-56 lg:w-160 lg:max-h-112"
+                />
+              </template>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 <style scoped>
 video::-webkit-media-controls-play-button {
-    display: none;
-    -webkit-appearance: none;
+  display: none;
+  -webkit-appearance: none;
 }
 
 video::-webkit-media-controls-panel {
-    display: none;
-    -webkit-appearance: none;
+  display: none;
+  -webkit-appearance: none;
 }
 </style>
