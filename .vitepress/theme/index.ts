6308b9963e776652dbbebc5c8fbcec5369183785
Jason Lee <huacnlee@gmail.com>
2025-12-18 16:33:35 +0800

    chore: Add GA.

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,26 +1,27 @@
-import { h } from 'vue'
-import DefaultTheme from 'vitepress/theme-without-fonts'
-import Footer from '../../docs/pages/Footer.vue'
+import { h } from "vue";
+import DefaultTheme from "vitepress/theme-without-fonts";
+import Footer from "../../docs/pages/Footer.vue";
 import mediumZoom from "medium-zoom";
 import { useRoute, useRouter } from "vitepress";
 import { onMounted, watch, nextTick } from "vue";
-import './custom.css'
-import 'virtual:uno.css'
+import "./custom.css";
+import "virtual:uno.css";
 import { MotionPlugin } from "@vueuse/motion";
-import { getLocaleByAppUA } from '../../docs/pages/utils';
-
+import { getLocaleByAppUA } from "../../docs/pages/utils";
+import googleAnalytics from "vitepress-plugin-google-analytics";
 
 export default {
   extends: DefaultTheme,
   Layout() {
-    return h(DefaultTheme.Layout, null,
-      {
-        "layout-bottom": () => h(Footer)
-      }
-    )
+    return h(DefaultTheme.Layout, null, {
+      "layout-bottom": () => h(Footer),
+    });
   },
   enhanceApp({ app }) {
     app.use(MotionPlugin);
+    googleAnalytics({
+      id: "G-P81Y8BDYYS",
+    });
   },
   setup() {
     // Support zooming images
@@ -41,5 +42,5 @@ export default {
     if (lang) {
       router.go(`/desktop/${lang}/`);
     }
-  }
-}
+  },
+};
